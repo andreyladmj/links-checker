1946b59917e42aa9b11790802abcec3c812afc53
Srivoknovski <test@gmail.com>
2018-10-19 07:35:59 +0300

    indexer

--- util/check_place_to_paid.py
+++ util/check_place_to_paid.py
@@ -1,32 +1,46 @@
 from time import sleep, time
 
 import requests
+from datetime import datetime
+
 
 def get_execution_time(start_time):
-    diff = int(time() - start_time)
-    return format_time(diff)
+    return strfdelta(datetime.now(), start_time)
 
 def format_time(time):
-    hours = time // 3600 % 24
-    minutes = time // 60 % 60
-    seconds = time % 60
-    return "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+    return time.strftime("%H:%M:%S.%f")
 
+def strfdelta(end, start):
+    diff = end - start
+    return "{}.{}s".format(diff.seconds, diff.microseconds)
 
 if __name__ == '__main__':
 
-    for i in range(10):
-        start_request_time = time()
-        session = requests.Session()
-        session.auth = ("test", "Q8UtsemJHZEJxOnE")
-        hostname = "https://place2paid-prediction.edusson-data-science.com"
-        response = session.get(hostname + '/api/v1/orders/1')
-        print("")
-        print(response)
-        print('Status code', response.status_code, 'Response', response.content)
-        print(response.content)
-        print('Request Time:', get_execution_time(start_request_time), 'Start Time', format_time(start_request_time), 'End Time', format_time(time()))
-        sleep(1)
+    while True:
+
+        print('UTC hour', datetime.utcnow(), datetime.utcnow().hour)
+        if (datetime.utcnow().hour >= 0 and datetime.utcnow().hour < 4) or datetime.utcnow().hour == 24:
+            start_request_time = datetime.utcnow()
+            session = requests.Session()
+            session.auth = ("test", "Q8UtsemJHZEJxOnE")
+            hostname = "https://place2paid-prediction.edusson-data-science.com"
+            response = session.get(hostname + '/api/v1/orders/1092872')
+
+            s = 'Get: {}, Code: {}, Request Time: {}, Start Time: {}, End Time: {}, Response: {}'.format(1092872,
+                response.status_code,
+                get_execution_time(start_request_time),
+                format_time(start_request_time),
+                format_time(datetime.utcnow()),
+                response.content)
+
+            print(s)
+
+            with open('hour-{}.txt'.format(datetime.utcnow().hour), 'a+') as f:
+                f.write(s + "\n")
+
+            sleep(0.75)
+        else:
+            sleep(10)
 
 # curl --user edusson:'+vju7VwmhNXz]9(.' https://place2paid-prediction.edusson-data-science.com/api/v1/orders/1092872
 # curl --insecure --user edusson:'+vju7VwmhNXz]9(.' https://dev.place2paid.edusson-data-science.com/api/v1/orders/1092872
